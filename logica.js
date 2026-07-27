6b6ef792e25ef5cc3157b0d903fad814e4f682f7
rochasantosmiguel-byte <rocha.santos.miguel@escola.pr.gov.br>
2026-07-27 12:26:56 -0300

    Update logica.js

--- logica.js
+++ logica.js
@@ -1,6 +1,83 @@
-let contador = 0;
-function clicker(){
-    let pontos = document.getElementById("pontos");
-    contador += 1
-    pontos.textContent = contador
+let score = 0;
+
+let clickPower = 1;
+
+let multiplier = 1;
+
+let autoClick = 0;
+
+const scoreText = document.getElementById("score");
+const powerText = document.getElementById("power");
+
+document.getElementById("clickBtn").onclick = function(){
+
+    score += cl
+
+    update();
 }
+
+function update(){
+
+    scoreText.innerHTML = score;
+    powerText.innerHTML = clickPower * multiplier;
+
+}
+
+function buyPower(){
+
+    if(score >= 50){
+
+        score -= 50;
+
+        clickPower++;
+
+        update();
+
+    }else{
+
+        alert("Pontos insuficientes!");
+    }
+
+}
+
+function buyMultiplier(){
+
+    if(score >= 200){
+
+        score -= 200;
+
+        multiplier *= 2;
+
+        update();
+
+    }else{
+
+        alert("Pontos insuficientes!");
+    }
+
+}
+
+function buyAutoClick(){
+
+    if(score >= 500){
+
+        score -= 500;
+
+        autoClick++;
+
+        update();
+
+    }else{
+
+        alert("Pontos insuficientes!");
+    }
+
+}
+
+setInterval(function(){
+
+    score += autoClick;
+
+    update();
+
+},1000);
